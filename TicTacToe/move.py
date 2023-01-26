c1f060e5a6d550966d0e3feba3441ee0904ebb7d
ndziwins <dziwinski.n@gmail.com>
2023-01-26 19:49:17 +0100

    CzyKoniecGry working version

--- TicTacToe/move.py
+++ TicTacToe/move.py
@@ -12,3 +12,23 @@ def zmianaGracza(gracz):
     elif gracz =='O':
         gracz = 'X'
     return gracz
+
+def czyKoniec(plansza):
+    if plansza[0][0] != "[ ]" and plansza[0][0] == plansza[0][1] and plansza[0][1] == plansza[0][2]:
+        return True
+    elif plansza[1][0] != "[ ]" and plansza[1][0] == plansza[1][1] and plansza[1][1] == plansza[1][2]:
+        return True
+    elif plansza[2][0] != "[ ]" and plansza[2][0] == plansza[2][1] and plansza[2][1] == plansza[2][2]:
+        return True
+    elif plansza[0][0] != "[ ]" and plansza[0][0] == plansza[1][0] and plansza[1][0] == plansza[2][0]:
+        return True
+    elif plansza[0][1] != "[ ]" and plansza[0][1] == plansza[1][1] and plansza[1][1] == plansza[2][1]:
+        return True
+    elif plansza[0][2] != "[ ]" and plansza[0][2] == plansza[1][2] and plansza[1][2] == plansza[2][2]:
+        return True
+    elif plansza[0][0] != "[ ]" and plansza[0][0] == plansza[1][1] and plansza[1][1] == plansza[2][2]:
+        return True
+    elif plansza[0][2] != "[ ]" and plansza[0][2] == plansza[1][1] and plansza[1][1] == plansza[2][0]:
+        return True
+    else:
+        return False
--- TicTacToe/tictactoe.py
+++ TicTacToe/tictactoe.py
@@ -1,9 +1,3 @@
-'''
-0 - puste pole
-1 - oznacza gracz x
-2 - oznacza gracz o
-'''
-
 from board import *
 from move import *
 
@@ -13,21 +7,11 @@ czyKoniecGry = False
 plansza = tworzPlansze()
 printPlansza(plansza, gracz)
 
-def czyKoniec(plansza):
-        if plansza[0][0] != "[ ]" and plansza[0][0] == plansza[0][1] and plansza[0][1] == plansza[0][2]:
-            return True
-        elif plansza[1][0] != "[ ]" and plansza[1][0] == plansza[1][1] and plansza[1][1] == plansza[1][2]:
-            return True
-        elif plansza[2][0] != "[ ]" and plansza[2][0] == plansza[2][1] and plansza[2][1] == plansza[2][2]:
-            return True
-        else:
-            return False
-
-
 while czyKoniecGry == False :
     kolumna = int(input("Podaj kolumnę do ruchu [0-2]: "))
     wiersz = int(input("Podaj wiersz do ruchu [0-2]: "))
     gracz = ruch(plansza, gracz, kolumna, wiersz)
     printPlansza(plansza, gracz)
     czyKoniecGry = czyKoniec(plansza)
-    
+    if czyKoniecGry:
+        print("HURRA! Wygrałeś!")
