2efc71ffadf4a10d6bc1db2e0dfc29438dedb77c
ndziwins <dziwinski.n@gmail.com>
2023-01-26 19:29:14 +0100

    Add czyKoniec v1

--- TicTacToe/tictactoe.py
+++ TicTacToe/tictactoe.py
@@ -13,32 +13,21 @@ czyKoniecGry = False
 plansza = tworzPlansze()
 printPlansza(plansza, gracz)
 
-kolumna = 0
-wiersz = 2
-gracz = ruch(plansza, gracz, kolumna, wiersz)
-printPlansza(plansza, gracz)
+def czyKoniec(plansza):
+        if plansza[0][0] != "[ ]" and plansza[0][0] == plansza[0][1] and plansza[0][1] == plansza[0][2]:
+            return True
+        elif plansza[1][0] != "[ ]" and plansza[1][0] == plansza[1][1] and plansza[1][1] == plansza[1][2]:
+            return True
+        elif plansza[2][0] != "[ ]" and plansza[2][0] == plansza[2][1] and plansza[2][1] == plansza[2][2]:
+            return True
+        else:
+            return False
 
-kolumna = 0
-wiersz = 1
-gracz = ruch(plansza, gracz, kolumna, wiersz)
-printPlansza(plansza, gracz)
 
-kolumna = 1
-wiersz = 1
-gracz = ruch(plansza, gracz, kolumna, wiersz)
-printPlansza(plansza, gracz)
-
-kolumna = 1
-wiersz = 1
-gracz = ruch(plansza, gracz, kolumna, wiersz)
-printPlansza(plansza, gracz)
-
-kolumna = 1
-wiersz = 0
-gracz = ruch(plansza, gracz, kolumna, wiersz)
-printPlansza(plansza, gracz)
-
-kolumna = 4
-wiersz = 1
-gracz = ruch(plansza, gracz, kolumna, wiersz)
-printPlansza(plansza, gracz)
+while czyKoniecGry == False :
+    kolumna = int(input("Podaj kolumnę do ruchu [0-2]: "))
+    wiersz = int(input("Podaj wiersz do ruchu [0-2]: "))
+    gracz = ruch(plansza, gracz, kolumna, wiersz)
+    printPlansza(plansza, gracz)
+    czyKoniecGry = czyKoniec(plansza)
+    
